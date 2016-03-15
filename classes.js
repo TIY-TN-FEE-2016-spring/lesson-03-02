35936e7e3e950c61b760b581a21153e77b0191b3
Ryan Tablada <ryan.tablada@gmail.com>
2016-03-15 11:55:26 -0500

    Function to return object & hides age w/o running interrogate

--- classes.js
+++ classes.js
@@ -29,3 +29,22 @@ console.log(marge.fullName());
 marge.lastName = 'Simpson';
 console.log(marge.fullName());
 console.log(marge.age); // Public Information
+
+var createPerson = function(firstName, lastName, age) {
+  return {
+    firstName, lastName,
+
+    interrogate() {
+      return age;
+    },
+
+    fullName: getFullName,
+  };
+}
+
+var bart = createPerson('Bart', 'Simpson', 12);
+
+console.log(bart.fullName());
+console.log(bart);
+console.log(bart.age); // This has been protected
+console.log(bart.interrogate());
